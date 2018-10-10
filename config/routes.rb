61f31a4770bb131f995e0a9915a9f40fc1048373
geoffrey woodcock <theatomproject@gmail.com>
2018-10-10 17:10:43 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'hello world', to: 'static#hell world'
+  get 'hello_world', to: 'static#hello_world'
   
   
   # The priority is based upon order of creation: first created -> highest priority.
